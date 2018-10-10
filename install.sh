b7bad5775e4d16a1e2b83a93087f01076356a2de
mike <mike@example.com>
2018-10-10 00:01:24 -0400

    working

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update
+sudo apt-get install python-pip
+
 sudo apt-get install python-pip
 sudo pip install flask
 sudo pip install requests
